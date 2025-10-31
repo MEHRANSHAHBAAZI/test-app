32403f9da378e220cde76816c09c3534b7af5587
MEHRANSHAHBAAZI <MEHRANSHAHBAAZI96@gmail.com>
2025-10-31 10:40:22 +0100

    Rename function sum to plus and update implementation to append numbers

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
 print("heloo world")
-def sum():
+def plus():
+    num3 = []
     num1 = int(input("enter your first number: "))
     num2 = int(input("enter your second number: "))
-    print (num1+num2)
-sum()
+    num3.append(num1)
+    num3.append(num2)
+    print (sum(num3))
+plus()
