2ccf467c807d2ab2bcf9e3ac094f855128ba23c6
MEHRANSHAHBAAZI <MEHRANSHAHBAAZI96@gmail.com>
2025-11-01 20:44:23 +0100

    variables are changed

--- python-candoo/create-txt-file.py
+++ python-candoo/create-txt-file.py
@@ -1,7 +1,7 @@
 from webbrowser import open_new
 for i in range (3):
-    var_name = input("enter your name:")
-    var_lastname = input("enter your last name:")
-    var_id = int(input("enter your id:"))
+    name = input("enter your name:")
+    lastname = input("enter your last name:")
+    id = int(input("enter your id:"))
     with open("data.txt", "a")as file:
-        file.write(f"you are {var_name} {var_lastname} and this is your ID: {var_id}\n")
+        file.write(f"you are {name} {lastname} and this is your ID: {id}\n")
